f5f6ade3517db078e4c6465d0b180ba428a2dd1e
Infarh <shmachilin@yandex.ru>
2021-07-11 00:48:09 +0300

    Именованные репозитории

---
+++ MathCore.Interfaces/Repositories/IEntityRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using MathCore.Interfaces.Entities;
+
+namespace MathCore.Interfaces.Repositories
+{
+    /// <summary>Репозиторий сущностей</summary>
+    /// <typeparam name="T">Тип сущности</typeparam>
+    /// <typeparam name="TKey">Тип первичного ключа сущности</typeparam>
+    public interface IEntityRepository<T, in TKey> : IRepository<T> where T : IEntity<TKey>
+    {
+        /// <summary>Существует ли сущность с указанным идентификатором</summary>
+        /// <param name="Id">Проверяемый идентификатор сущности</param>
+        /// <param name="Cancel">Признак отмены асинхронной операции</param>
+        /// <returns>Истина, если сущность с указанным идентификатором существует в репозитории</returns>
+        Task<bool> ExistId(TKey Id, CancellationToken Cancel = default);
+
+        /// <summary>Получить сущность по идентификатору</summary>
+        /// <param name="Id">Идентификатор сущности</param>
+        /// <param name="Cancel">Признак отмены асинхронной операции</param>
+        /// <returns>Сущность с указанным идентификатором, либо null в случае её отсутствия</returns>
+        Task<T> GetById(TKey Id, CancellationToken Cancel = default);
+
+        /// <summary>Удалить сущность по её идентификатору</summary>
+        /// <param name="id">Идентификатор удаляемой сущности</param>
+        /// <param name="Cancel">Признак отмены асинхронной операции</param>
+        /// <returns>Удалённая из репозитория сущность</returns>
+        Task<T> DeleteById(TKey id, CancellationToken Cancel = default);
+    }
+
+    /// <summary>Репозиторий сущностей</summary>
+    /// <typeparam name="T">Тип сущности</typeparam>
+    public interface IEntityRepository<T> : IEntityRepository<T, int> where T : IEntity { }
+}
---
+++ MathCore.Interfaces/Repositories/INamedEntityRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using MathCore.Interfaces.Entities;
+
+namespace MathCore.Interfaces.Repositories
+{
+    /// <summary>Репозиторий именованных сущностей</summary>
+    /// <typeparam name="T">Тип сущности</typeparam>
+    /// <typeparam name="TKey">Тип первичного ключа сущности</typeparam>
+    public interface INamedEntityRepository<T, in TKey> : IEntityRepository<T, TKey> where T : INamedEntity<TKey>
+    {
+        /// <summary>Проверка наличия сущности по её имени</summary>
+        /// <param name="Name">Проверяемое имя сущности</param>
+        /// <param name="Cancel">Признак отмены асинхронной операции</param>
+        /// <returns>Истина, если сущность с указанным именем существует в репозитории</returns>
+        Task<bool> ExistName(string Name, CancellationToken Cancel = default);
+
+        /// <summary>Получение сущности по её имени</summary>
+        /// <param name="Name">Имя сущности, которое надо получить</param>
+        /// <param name="Cancel">Признак отмены асинхронной операции</param>
+        /// <returns>Сущность с указанным именем</returns>
+        Task<T> GetByName(string Name, CancellationToken Cancel = default);
+
+        /// <summary>Удаление сущности с указанным именем</summary>
+        /// <param name="Name">Имя сущности, которую надо удалить из репозитория</param>
+        /// <param name="Cancel">Признак отмены асинхронной операции</param>
+        /// <returns>Удалённая из репозитория сущность, либо null, если сущность не найдена</returns>
+        Task<T> DeleteByName(string Name, CancellationToken Cancel = default);
+    }
+
+    /// <summary>Репозиторий именованных сущностей</summary>
+    /// <typeparam name="T">Тип сущности</typeparam>
+    public interface INamedEntityRepository<T> : INamedEntityRepository<T, int> where T : INamedEntity { }
+
+}
